dc5fbc034b7ed5e86e7f4d105be152854edd275c
Kauan Peçanha <kauanpecanha1@gmail.com>
2023-12-27 18:20:11 -0300

    Parte 3 - Incorporação do Insomnia

---
+++ part3.js
@@ -0,0 +1,71 @@
+// aula 2 - consumindo os parâmetros query e route
+
+const express = require("express")
+
+const server = express()
+
+// query params: ?nome=Kauan&idade=19
+
+// route params: http://localhost:5000/hello/
+
+// chamada completa: http://localhost:5000/hello?nome=Kauan&idade=19
+
+server.get("/", (req, res) => {
+    
+    // const nome = req.query.nome;
+
+    // const idade = req.query.idade;
+
+    const { nome, idade } = req.query;
+
+    return res.json({
+        title: "Home",
+        message: `Olá, ${nome}, você tem ${idade} anos.`
+    })
+
+})
+
+// mensagem geral
+server.get("/customers", (req, res) => {
+    
+    const nome = req.params.nome;
+    
+    // const idade = req.params.idade;
+
+    return res.json({
+        title: "Home",
+        message: `Olá!`
+    })
+
+})
+
+// mensagem específica
+server.get("/customers/:nome", (req, res) => {
+    
+    const nome = req.params.nome;
+    
+    // const idade = req.params.idade;
+
+    return res.json({
+        title: "Home",
+        message: `Olá, ${nome}!`
+    })
+
+})
+
+server.get("/customers/:nome/:idade", (req, res) => {
+    
+    const nome = req.params.nome;
+    
+    const idade = req.params.idade;
+    
+    // const idade = req.params.idade;
+
+    return res.json({
+        title: "Home",
+        message: `Olá, ${nome}, você tem ${idade}!`
+    })
+
+})
+
+server.listen(5000)
